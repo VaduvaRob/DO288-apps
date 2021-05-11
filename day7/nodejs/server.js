974904e3127da124cf8276e17ea8bd2bc7cd72bb
VaduvaRob <83640048+VaduvaRob@users.noreply.github.com>
2021-05-11 17:38:10 +0300

    Update server.js

--- day7/nodejs/server.js
+++ day7/nodejs/server.js
@@ -5,13 +5,6 @@ process.on('uncaughtException', (err) => {
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req, res) =>{
-var con = mysql.createConnection({
-});
-con.connect(function(err) {
- if (err) throw err;
- console.log("Connected!");
-});
 con.query('SELECT * from test2', function (err, rows, fields) {
  if (err) res.send(err)
  console.log('The solution is: ', rows)
