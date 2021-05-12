d1b711b085a3ad5cb27b3f54c060d3eb2198501e
VaduvaRob <83640048+VaduvaRob@users.noreply.github.com>
2021-05-12 13:11:09 +0300

    Update server.js

--- day7/nodejs/server.js
+++ day7/nodejs/server.js
@@ -6,6 +6,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
+var con = mysql.createConnection({
+ host: process.env.host ,
+ user: process.env.user ,
+ password: process.env.password ,
+ database: process.env.database 
+});
+con.connect(function(err) {
+ if (err) throw err;
+ console.log("Connected!");
+});
 con.query('SELECT * from test2', function (err, rows, fields) {
  if (err) res.send(err)
  console.log('The solution is: ', rows)
